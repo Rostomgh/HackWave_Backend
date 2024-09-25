5f774ffdfc09b57e57c9030add3774ec7bbdce60
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-09-25 21:19:56 +0100

    fix changes

--- Models/User.js
+++ Models/User.js
@@ -1,24 +1,6 @@
-const mongoose = require("mongoose");
-<<<<<<< HEAD
-const Schema= mongoose.Schema
-const AgentSchema = new Schema({
-   
-  name:{type:String,required:true},
-  email:{type:String,required:true},
-  password: {
-    type: String,
-    minlength: [8, "Password should be at least 8 characters"],
-    required: true,
-  },
-  role:{type:String,required:false},
-  },
-  {
-    timestamps: true  
-  });
-const AgentModel= mongoose.model('AgentSchema', AgentSchema)
-module.exports=AgentModel
+
   
-=======
+
 const Schema = mongoose.Schema;
 const AgentSchema = new Schema(
   {
@@ -37,4 +19,4 @@ const AgentSchema = new Schema(
 );
 const AgentModel = mongoose.model("AgentSchema", AgentSchema);
 module.exports = AgentModel;
->>>>>>> 64eb25a23d93b923c418ba00ea6f384a056f420d
+
